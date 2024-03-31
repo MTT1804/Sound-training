f4e2330ddb8a8ef18e698dbcace3baa5ac545488
MateuszTyl <mateusz.tyl.stud@pw.edu.pl>
2024-03-31 23:12:15 +0200

    Reading filenames and adding to button system partly finished

--- app/src/main/java/com/example/soundtraining/Sound.java
+++ app/src/main/java/com/example/soundtraining/Sound.java
@@ -1,6 +1,8 @@
 package com.example.soundtraining;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,9 +15,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Random;
+
 public class Sound extends Fragment {
-    View mainView;
-    int m, n;
+    private View mainView;
+    private String [] soundNames;
+    private Integer [] choices;
+    private int m, n;
     public Sound(int m, int n) {
         super(R.layout.sound);
         this.m = m;
@@ -26,8 +34,41 @@ public class Sound extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mainView = inflater.inflate(R.layout.sound, container, false);
+        searchForSoundsAndGetSoundsNames();
         insertAParticularNumberOfButtonsIntoGridLayout(m,n);
         return mainView;
+    }
+
+    public void generateAndInsertRandomlyChosenSounds(int fieldAmount) {
+        choices = generateRandomChoices(fieldAmount);
+        for (int buttonCounter = 0; buttonCounter < fieldAmount; buttonCounter++) {
+            int buttonId = getResources().getIdentifier("button_" + buttonCounter, "id", mainView.getContext().getPackageName());
+            ((Button)mainView.findViewById(buttonId)).setText(soundNames[choices[buttonCounter]]);
+        }
+    }
+
+    public Integer [] generateRandomChoices(int fieldAmount){
+        Random r = new Random();
+        HashSet<Integer>numbers = new HashSet<>();
+
+        while (fieldAmount != numbers.size()) {
+            for (int i = 0; i < soundNames.length; i++){
+                numbers.add(r.nextInt(fieldAmount));
+            }
+        }
+        return numbers.toArray(new Integer[numbers.size()]);
+    }
+    public void searchForSoundsAndGetSoundsNames(){
+        AssetManager assetManager = mainView.getContext().getAssets();
+        try {
+            soundNames = assetManager.list("sounds");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (int i = 0; i < soundNames.length; i++){
+            soundNames[i] = soundNames[i].replace(".ogg", "");
+        }
+
     }
 
     public void insertAParticularNumberOfButtonsIntoGridLayout(int m, int n) {
@@ -39,10 +80,12 @@ public class Sound extends Fragment {
             public void onGlobalLayout() {
                 int buttonWidth = buttonsMatrix.getWidth() / n;
                 int buttonHeight = buttonsMatrix.getHeight() / m;
-
+                int buttonCounter = 0;
                 for (int i = 0; i < m; i++) {
                     for (int j = 0; j < n; j++) {
                         Button button = new Button(mainView.getContext());
+                        int buttonId = getResources().getIdentifier("button_" + buttonCounter, "id", mainView.getContext().getPackageName());
+                        button.setId(buttonId);
                         button.setText("Przycisk " + ((i * n) + j + 1));
                         GridLayout.LayoutParams params = new GridLayout.LayoutParams();
                         params.width = buttonWidth;
@@ -51,8 +94,10 @@ public class Sound extends Fragment {
                         params.setGravity(Gravity.FILL);
                         button.setLayoutParams(params);
                         buttonsMatrix.addView(button);
+                        buttonCounter++;
                     }
                 }
+                generateAndInsertRandomlyChosenSounds(m*n);
                 buttonsMatrix.getViewTreeObserver().removeOnGlobalLayoutListener(this);
             }
         });
